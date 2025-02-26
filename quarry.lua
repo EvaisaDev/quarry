b8626607fe2e3e7c4560123e194a3c2bc04029f9
Evaisa <evagiacosa1@gmail.com>
2025-02-26 14:30:54 +0100

    Update quarry.lua

--- quarry.lua
+++ quarry.lua
@@ -19,307 +19,255 @@ local USRINTERRUPT = 6
 local CHARCOALONLY = false
 local USEMODEM = false
 
+local FUEL_SOURCES = {
+ ["minecraft:charcoal"] = true,
+ ["minecraft:coal"] = true
+}
 
--- Arguments
-local tArgs = {...}
-for i=1,#tArgs do
-	local arg = tArgs[i]
-	if string.find(arg, "-") == 1 then
-		for c=2,string.len(arg) do
-			local ch = string.sub(arg,c,c)
-			if ch == 'c' then
-				CHARCOALONLY = true
-			elseif ch == 'm' then
-				USEMODEM = true
-			else
-				write("Invalid flag '")
-				write(ch)
-				print("'")
-			end
-		end
-	end
+function isFuelItem(item)
+ if item == nil then
+  return false
+ end
+ if not FUEL_SOURCES[item.name] then
+  return false
+ end
+ if CHARCOALONLY and item.name == "minecraft:coal" and item.damage == 0 then
+  return false
+ end
+ return true
 end
 
+local tArgs = {...}
+for i=1,#tArgs do
+ local arg = tArgs[i]
+ if string.find(arg, "-") == 1 then
+  for c=2,string.len(arg) do
+   local ch = string.sub(arg,c,c)
+   if ch == 'c' then
+    CHARCOALONLY = true
+   elseif ch == 'm' then
+    USEMODEM = true
+   else
+    write("Invalid flag '")
+    write(ch)
+    print("'")
+   end
+  end
+ end
+end
 
 function out(s)
-
-	s2 = s .. " @ [" .. x .. ", " .. y .. ", " .. z .. "]"
-			
-	print(s2)
-	if USEMODEM then
-		rednet.broadcast(s2, "miningTurtle")
-	end  
+ s2 = s .. " @ [" .. x .. ", " .. y .. ", " .. z .. "]"
+ print(s2)
+ if USEMODEM then
+  rednet.broadcast(s2, "miningTurtle")
+ end
 end
 
 function dropInChest()
-	local turns = 0
-	while turns < 4 do
-		local success, data = turtle.inspect()
-		if success and data.name == "minecraft:chest" then
-			out("Dropping items in chest")
-			for i=1,16 do
-				turtle.select(i)
-				local d = turtle.getItemDetail()
-				if d ~= nil and d.name ~= "minecraft:charcoal" and (d.name == "minecraft:coal" and CHARCOALONLY == false) == false and (d.damage == nil or d.name..d.damage ~= "minecraft:coal1") then
-					turtle.drop()
-				end
-			end
-			for i=1,turns do
-				turtle.turnRight()
-			end
-			return
-		end
-		turtle.turnLeft()
-		turns = turns + 1
-	end
+ local turns = 0
+ while turns < 4 do
+  local success, data = turtle.inspect()
+  if success and data.name == "minecraft:chest" then
+   out("Dropping items in chest")
+   for i=1,16 do
+    turtle.select(i)
+    local d = turtle.getItemDetail()
+    if d ~= nil and not isFuelItem(d) then
+     turtle.drop()
+    end
+   end
+   for i=1,turns do
+    turtle.turnRight()
+   end
+   return
+  end
+  turtle.turnLeft()
+  turns = turns + 1
+ end
 end
-   
 
 function goDown()
-	while true do
-		if turtle.getFuelLevel() <= fuelNeededToGoBack() then
-			if not refuel() then
-				return OUTOFFUEL
-			end
-		end
-	
-		if not turtle.down() then
-			turtle.up()
-			z = z+1
-			return
-		end
-		z = z-1
-	end
+ while true do
+  if turtle.getFuelLevel() <= fuelNeededToGoBack() then
+   if not refuel() then
+    return OUTOFFUEL
+   end
+  end
+  if not turtle.down() then
+   turtle.up()
+   z = z+1
+   return
+  end
+  z = z-1
+ end
 end
 
 function fuelNeededToGoBack()
-	return -z + x + y + 2
+ return -z + x + y + 2
 end
 
 function refuel()
-	for i=1, 16 do
-		-- Only run on Charcoal
-		turtle.select(i)
-		
-		item = turtle.getItemDetail()
-		if item and
-				(item.name == "minecraft:charcoal" or (item.name == "minecraft:coal" and
-				(CHARCOALONLY == false or item.damage == 1))) and
-				turtle.refuel(1) then
-			return true
-		end
-	end
-	
-	return false
+ for i=1,16 do
+  turtle.select(i)
+  item = turtle.getItemDetail()
+  if isFuelItem(item) and turtle.refuel(1) then
+   return true
+  end
+ end
+ return false
 end
 
 function moveH()
-	if inv.isInventoryFull() then
-		out("Dropping thrash")
-		inv.dropThrash()
-		
-		if inv.isInventoryFull() then
-			out ("Stacking items")
-			inv.stackItems()
-		end
-		
-		if inv.isInventoryFull() then
-			out("Full inventory!")
-			return FULLINV  
-		end
-	end
-	
-	if turtle.getFuelLevel() <= fuelNeededToGoBack() then
-		if not refuel() then
-			out("Out of fuel!")
-			return OUTOFFUEL
-		end
-	end
-	
-	if facingfw and y<max-1 then
-	-- Going one way
-		local dugFw = t.dig()
-		if dugFw == false then
-			out("Hit bedrock, can't keep going")
-			return BLOCKEDMOV
-		end
-		t.digUp()
-		t.digDown()
-	
-		if t.fw() == false then
-			return BLOCKEDMOV
-		end
-		
-		y = y+1
-	
-	elseif not facingfw and y>0 then
-	-- Going the other way
-		t.dig()
-		t.digUp()
-		t.digDown()
-		
-		if t.fw() == false then
-			return BLOCKEDMOV
-		end
-		
-		y = y-1
-		
-	else
-		if x+1 >= max then
-			t.digUp()
-			t.digDown()
-			return LAYERCOMPLETE -- Done with this Y level
-		end
-		
-		-- If not done, turn around
-		if facingfw then
-			turtle.turnRight()
-		else
-			turtle.turnLeft()
-		end
-		
-		t.dig()
-		t.digUp()
-		t.digDown()
-		
-		if t.fw() == false then
-			return BLOCKEDMOV
-		end
-		
-		x = x+1
-		
-		if facingfw then
-			turtle.turnRight()
-		else
-			turtle.turnLeft()
-		end
-		
-		facingfw = not facingfw
-	end
-	
-	return OK
+ if inv.isInventoryFull() then
+  out("Dropping thrash")
+  inv.dropThrash()
+  if inv.isInventoryFull() then
+   out ("Stacking items")
+   inv.stackItems()
+  end
+  if inv.isInventoryFull() then
+   out("Full inventory!")
+   return FULLINV
+  end
+ end
+ if turtle.getFuelLevel() <= fuelNeededToGoBack() then
+  if not refuel() then
+   out("Out of fuel!")
+   return OUTOFFUEL
+  end
+ end
+ if facingfw and y<max-1 then
+  local dugFw = t.dig()
+  if dugFw == false then
+   out("Hit bedrock, can't keep going")
+   return BLOCKEDMOV
+  end
+  t.digUp()
+  t.digDown()
+  if t.fw() == false then
+   return BLOCKEDMOV
+  end
+  y = y+1
+ elseif not facingfw and y>0 then
+  t.dig()
+  t.digUp()
+  t.digDown()
+  if t.fw() == false then
+   return BLOCKEDMOV
+  end
+  y = y-1
+ else
+  if x+1 >= max then
+   t.digUp()
+   t.digDown()
+   return LAYERCOMPLETE
+  end
+  if facingfw then
+   turtle.turnRight()
+  else
+   turtle.turnLeft()
+  end
+  t.dig()
+  t.digUp()
+  t.digDown()
+  if t.fw() == false then
+   return BLOCKEDMOV
+  end
+  x = x+1
+  if facingfw then
+   turtle.turnRight()
+  else
+   turtle.turnLeft()
+  end
+  facingfw = not facingfw
+ end
+ return OK
 end
 
 function digLayer()
-	
-	local errorcode = OK
-
-	while errorcode == OK do
-		if USEMODEM then
-			local msg = rednet.receive(1)
-			if msg ~= nil and string.find(msg, "return") ~= nil then
-				return USRINTERRUPT
-			end
-		end
-		errorcode = moveH()
-	end
-	
-	if errorcode == LAYERCOMPLETE then
-		return OK
-	end
-	
-	return errorcode  
+ local errorcode = OK
+ while errorcode == OK do
+  if USEMODEM then
+   local msg = rednet.receive(1)
+   if msg ~= nil and string.find(msg, "return") ~= nil then
+    return USRINTERRUPT
+   end
+  end
+  errorcode = moveH()
+ end
+ if errorcode == LAYERCOMPLETE then
+  return OK
+ end
+ return errorcode
 end
 
 function goToOrigin()
-	
-	if facingfw then
-		
-		turtle.turnLeft()
-		
-		t.fw(x)
-		
-		turtle.turnLeft()
-		
-		t.fw(y)
-		
-		turtle.turnRight()
-		turtle.turnRight()
-		
-	else
-		
-		turtle.turnRight()
-		
-		t.fw(x)
-		
-		turtle.turnLeft()
-		
-		t.fw(y)
-		
-		turtle.turnRight()
-		turtle.turnRight()
-		
-	end
-	
-	x = 0
-	y = 0
-	facingfw = true
-	
+ if facingfw then
+  turtle.turnLeft()
+  t.fw(x)
+  turtle.turnLeft()
+  t.fw(y)
+  turtle.turnRight()
+  turtle.turnRight()
+ else
+  turtle.turnRight()
+  t.fw(x)
+  turtle.turnLeft()
+  t.fw(y)
+  turtle.turnRight()
+  turtle.turnRight()
+ end
+ x = 0
+ y = 0
+ facingfw = true
 end
 
 function goUp()
-
-	while z < 0 do
-		
-		t.up()
-		
-		z = z+1
-		
-	end
-	
-	goToOrigin()
-	
+ while z < 0 do
+  t.up()
+  z = z+1
+ end
+ goToOrigin()
 end
 
 function mainloop()
-
-	while true do
-
-		local errorcode = digLayer()
-	
-		if errorcode ~= OK then
-			goUp()
-			return errorcode
-		end
-		
-		goToOrigin()
-		
-		for i=1, 3 do
-			t.digDown()
-			success = t.down()
-		
-			if not success then
-				goUp()
-				return BLOCKEDMOV
-			end
-
-			z = z-1
-			out("Z: " .. z)
-
-		end
-	end
+ while true do
+  local errorcode = digLayer()
+  if errorcode ~= OK then
+   goUp()
+   return errorcode
+  end
+  goToOrigin()
+  for i=1, 3 do
+   t.digDown()
+   success = t.down()
+   if not success then
+    goUp()
+    return BLOCKEDMOV
+   end
+   z = z-1
+   out("Z: " .. z)
+  end
+ end
 end
 
 if USEMODEM then
-	rednet.open("right")
+ rednet.open("right")
 end
 
 out("\n\n\n-- WELCOME TO THE MINING TURTLE --\n\n")
-
 dropInChest()
 
 while true do
-
-	goDown()
-
-	local errorcode = mainloop()
-	dropInChest()
-	
-	if errorcode ~= FULLINV then
-		break
-	end
+ goDown()
+ local errorcode = mainloop()
+ dropInChest()
+ if errorcode ~= FULLINV then
+  break
+ end
 end
 
 if USEMODEM then
-	rednet.close("right")
+ rednet.close("right")
 end
